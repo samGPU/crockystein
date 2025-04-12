fc9133ef552c14cd839027db80e4ae2433a3b201
Sam <samgpu@proton.me>
2025-04-12 12:02:17 +0100

    Refactor ammo management: move ammo variables to global script and update input handling for weapon actions

---
+++ global.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var pistol_ammo = 10
+var rifle_ammo = 30000
+var chain_ammo = 20000
+var current_weapon = "knife" # Default weapon
--- ui.gd
+++ ui.gd
@@ -1,10 +1,5 @@
 extends CanvasLayer
 
-var pistol_ammo = 10
-var rifle_ammo = 30000
-var chain_ammo = 20000
-var current_weapon = "knife" # Default weapon
-
 var is_mouse_pressed = false
 
 # Called when the node enters the scene tree for the first time.
@@ -14,33 +9,33 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		if current_weapon == "knife" and not is_mouse_pressed:
+		if Global.current_weapon == "knife" and not is_mouse_pressed:
 			$AnimatedSprite2D.play("knife_stab")
-		elif current_weapon == "pistol" and not is_mouse_pressed:
-			if pistol_ammo > 0:
+		elif Global.current_weapon == "pistol" and not is_mouse_pressed:
+			if Global.pistol_ammo > 0:
 				$AnimatedSprite2D.play("pistol_shoot")
-				pistol_ammo -= 1
-			print("Pistol ammo remaining: ", pistol_ammo)
-		elif current_weapon == "rifle":
-			if rifle_ammo > 0:
+				Global.pistol_ammo -= 1
+			print("Pistol ammo remaining: ", Global.pistol_ammo)
+		elif Global.current_weapon == "rifle":
+			if Global.rifle_ammo > 0:
 				$AnimatedSprite2D.play("rifle_shoot")
-				rifle_ammo -= 1
-			print("Rifle ammo remaining: ", rifle_ammo)
-		elif current_weapon == "chain":
-			if chain_ammo > 0:
+				Global.rifle_ammo -= 1
+			print("Rifle ammo remaining: ", Global.rifle_ammo)
+		elif Global.current_weapon == "chain":
+			if Global.chain_ammo > 0:
 				$AnimatedSprite2D.play("chain_shoot")
-				chain_ammo -= 1
-			print("Chain ammo remaining: ", chain_ammo)
+				Global.chain_ammo -= 1
+			print("Chain ammo remaining: ", Global.chain_ammo)
 		is_mouse_pressed = true
 	else:
 		is_mouse_pressed = false
 
 func _on_AnimatedSprite2D_animation_finished():
-	if current_weapon == "knife":
+	if Global.current_weapon == "knife":
 		$AnimatedSprite2D.play("knife_idle")
-	elif current_weapon == "pistol":
+	elif Global.current_weapon == "pistol":
 		$AnimatedSprite2D.play("pistol_idle")
-	elif current_weapon == "rifle":
+	elif Global.current_weapon == "rifle":
 		$AnimatedSprite2D.play("rifle_idle")
-	elif current_weapon == "chain":
+	elif Global.current_weapon == "chain":
 		$AnimatedSprite2D.play("chain_idle")
